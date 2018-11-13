d12e6778b747e93315bc3eb9b927a6b3211defdd
sunsunyoung2jjang <jyp4703@nate.com>
2018-11-13 23:39:43 +0900

    Add global variable and #define

--- myas.h
+++ myas.h
@@ -2,5 +2,14 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define MEM 19
+#define REG 20
+#define IMM 21
+#define ADD 22
+
+int ex1, ex2;
+
+char source[256], destination[256];
+
 int instr_trans(char *op,char *args,char* mcode);
 int is_valid(char *op,char *args);
